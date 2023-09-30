defb1c56ec701da09323fbdd130b88b451e2c7fb
Mityasaurus <dmitriybovdun5@gmail.com>
2023-09-30 20:59:37 +0300

    Completed, but needs fixing

--- ClientUI/Form1.Designer.cs
+++ ClientUI/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnConnect = new Button();
+            label1 = new Label();
+            tb_IP = new TextBox();
+            label2 = new Label();
+            tb_Port = new TextBox();
+            btnGetQuote = new Button();
+            tb_quote = new TextBox();
+            SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(471, 12);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(140, 23);
+            btnConnect.TabIndex = 0;
+            btnConnect.Text = "Connect";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 1;
+            label1.Text = "IP";
+            // 
+            // tb_IP
+            // 
+            tb_IP.Location = new Point(35, 12);
+            tb_IP.Name = "tb_IP";
+            tb_IP.Size = new Size(166, 23);
+            tb_IP.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(250, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Port";
+            // 
+            // tb_Port
+            // 
+            tb_Port.Location = new Point(285, 13);
+            tb_Port.Name = "tb_Port";
+            tb_Port.Size = new Size(124, 23);
+            tb_Port.TabIndex = 4;
+            // 
+            // btnGetQuote
+            // 
+            btnGetQuote.Location = new Point(35, 70);
+            btnGetQuote.Name = "btnGetQuote";
+            btnGetQuote.Size = new Size(576, 23);
+            btnGetQuote.TabIndex = 5;
+            btnGetQuote.Text = "Get random quote";
+            btnGetQuote.UseVisualStyleBackColor = true;
+            btnGetQuote.Click += btnGetQuote_Click;
+            // 
+            // tb_quote
+            // 
+            tb_quote.Enabled = false;
+            tb_quote.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_quote.Location = new Point(35, 120);
+            tb_quote.Multiline = true;
+            tb_quote.Name = "tb_quote";
+            tb_quote.Size = new Size(576, 179);
+            tb_quote.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(669, 328);
+            Controls.Add(tb_quote);
+            Controls.Add(btnGetQuote);
+            Controls.Add(tb_Port);
+            Controls.Add(label2);
+            Controls.Add(tb_IP);
+            Controls.Add(label1);
+            Controls.Add(btnConnect);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnConnect;
+        private Label label1;
+        private TextBox tb_IP;
+        private Label label2;
+        private TextBox tb_Port;
+        private Button btnGetQuote;
+        private TextBox tb_quote;
     }
 }
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace ServerApp
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            var server = new TcpListener(IPAddress.Any, 8888);
+            var quotes = new List<string>
+            {
+                "Два самых важных дня в вашей жизни - день, когда вы родились, и день, когда вы поняли, зачем.",
+                "Величайший характеристикой человека является его способность быть настоящим с самим собой.",
+                "Будущее зависит от того, что вы делаете сегодня.",
+                "Самая невероятная машина, которой вы когда-либо управляли, - ваше собственное тело.",
+                "Единственное место, где успех и работа идут вместе перед работой - в словаре.",
+                "Жизнь - это что-то, что случается, когда вы заняты другими планами.",
+                "Ваше время ограничено, не тратьте его, живя чужой жизнью.",
+                "Последнее, что вы хотите, - это дойти до конца жизни и понять, что вы прожили ее в соответствии с чужими ожиданиями.",
+                "Наибольший триумф - это тот, который приходит после самой большой борьбы.",
+                "Будьте изменением, которое вы хотите видеть в мире."
+            };
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server started. Waiting for connections...");
+
+                while(true)
+                {
+                    using var client = await server.AcceptTcpClientAsync();
+                    Console.WriteLine("Client connected");
+                    var stream = client.GetStream();
+
+                    using var streamReader = new StreamReader(stream);
+                    using var streamWriter = new StreamWriter(stream);
+
+                    Random rand = new Random();
+                    string responce = quotes[rand.Next(0, quotes.Count)];
+
+                    await streamWriter.WriteLineAsync(responce);
+                    await streamWriter.FlushAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
         }
     }
 }
