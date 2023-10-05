a3f6353ce06f5e615fcd1cf88a94b34cb62b5903
Mityasaurus <dmitriybovdun5@gmail.com>
2023-10-05 20:22:29 +0300

    Completed tasks 1,2 and 3

---
+++ ClientApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+
+namespace ClientApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using TcpClient tcpClient = new TcpClient();
+            try
+            {
+                await tcpClient.ConnectAsync("127.0.0.1", 8888);
+                var stream = tcpClient.GetStream();
+
+                using var streamReader = new StreamReader(stream);
+                using var streamWriter = new StreamWriter(stream);
+
+                while (true)
+                {
+                    string request = Console.ReadLine();
+
+                    await streamWriter.WriteLineAsync(request);
+                    await streamWriter.FlushAsync();
+
+                    var responce = await streamReader.ReadLineAsync();
+                    Console.Clear();
+                    Console.WriteLine(responce);
+
+                    if (request.ToLower() == "end")
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                tcpClient.Close();
+            }
+        }
+    }
+}
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,10 +1,21 @@
-﻿using System.Net;
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
 using System.Net.Sockets;
 
 namespace ServerApp
 {
     internal class Program
     {
+        private static int currentActiveUsers = 0;
+
+        private static readonly ILogger logger = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        }).CreateLogger<Program>();
+
+        private static readonly int maxQuotes = 5;
+        private static readonly int maxUsers = 3;
+
         static async Task Main(string[] args)
         {
             var server = new TcpListener(IPAddress.Any, 8888);
@@ -27,22 +38,12 @@ namespace ServerApp
                 server.Start();
                 Console.WriteLine("Server started. Waiting for connections...");
 
-                while(true)
+                while (true)
                 {
-                    using var client = await server.AcceptTcpClientAsync();
-                    Console.WriteLine("Client connected");
-                    var stream = client.GetStream();
-
-                    using var streamReader = new StreamReader(stream);
-                    using var streamWriter = new StreamWriter(stream);
-
-                    Random rand = new Random();
-                    string responce = quotes[rand.Next(0, quotes.Count)];
-
-                    await streamWriter.WriteLineAsync(responce);
-                    await streamWriter.FlushAsync();
+                    var client = await server.AcceptTcpClientAsync();
+                    currentActiveUsers++;
+                    Task.Run(() => HandleClientAsync(client, quotes));
                 }
-
             }
             catch (Exception ex)
             {
@@ -52,6 +53,70 @@ namespace ServerApp
             {
                 server.Stop();
             }
+        }
+
+        static async Task HandleClientAsync(TcpClient client, List<string> quotes)
+        {
+            var clientEndPoint = client.Client.RemoteEndPoint.ToString();
+
+            logger.LogInformation($"Client connected: {clientEndPoint} at {DateTime.Now}");
+
+            var stream = client.GetStream();
+
+            using var streamReader = new StreamReader(stream);
+            using var streamWriter = new StreamWriter(stream);
+
+
+
+            int counter = 0;
+
+            while (true)
+            {
+                var request = await streamReader.ReadLineAsync();
+                if (request.ToLower() == "end")
+                {
+                    break;
+                }
+
+                string responce;
+
+                if (currentActiveUsers > maxUsers)
+                {
+                    responce = "The server is overloaded. Try to connect later.";
+
+                    await streamWriter.WriteLineAsync(responce);
+                    await streamWriter.FlushAsync();
+
+                    logger.LogWarning($"Users limit({maxUsers}) reached. Disconnecting {clientEndPoint}.");
+
+                    break;
+                }
+
+                if (counter >= maxQuotes)
+                {
+                    responce = "You have reached the maximum number of requests allowed. Closing the connection...";
+
+                    await streamWriter.WriteLineAsync(responce);
+                    await streamWriter.FlushAsync();
+
+                    logger.LogInformation($"Quote limit reached for {clientEndPoint}. Closing connection.");
+
+                    break;
+                }
+
+                Random rand = new Random();
+                responce = quotes[rand.Next(0, quotes.Count)];
+
+                await streamWriter.WriteLineAsync(responce);
+                await streamWriter.FlushAsync();
+
+                logger.LogInformation($"Sent quote to {clientEndPoint}: {responce}");
+                counter++;
+            }
+
+            client.Close();
+            currentActiveUsers--;
+            logger.LogInformation($"Client disconnected: {clientEndPoint} at {DateTime.Now}");
         }
     }
 }
